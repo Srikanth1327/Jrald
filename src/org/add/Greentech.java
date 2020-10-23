642c5a5a6f8b9a0581036e5b0d05a3a42bc03486
KICKdoll27 <srikanthrgvm@gmail.com>
2020-10-23 18:19:46 +0530

    done my coding

---
+++ src/org/add/Greentech.java
@@ -0,0 +1,28 @@
+package org.add;
+
+import java.util.Scanner;
+
+public class Greentech {
+
+public static void main(String[] args) {
+	
+	int a = 10;
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Please enter your atm pin no");
+	
+		  int atmpin = sc.nextInt();
+			System.out.println("the atmpin no is" + atmpin);
+			
+			
+			System.out.println("enter your salery");
+			
+			float sal = sc.nextFloat();
+			System.out.println(sal);
+			
+}
+		
+	
+			
+	}
+
